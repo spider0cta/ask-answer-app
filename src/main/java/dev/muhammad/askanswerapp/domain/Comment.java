3d7fb4cf32951f0ea5d9f922e461a1aed8d25c19
Urodacus <muhammadalaidy@protonmail.com>
2021-07-19 14:05:50 +0200

    Refactored Domain entities Using project Lombok

--- src/main/java/dev/muhammad/askanswerapp/domain/Comment.java
+++ src/main/java/dev/muhammad/askanswerapp/domain/Comment.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 @Data
