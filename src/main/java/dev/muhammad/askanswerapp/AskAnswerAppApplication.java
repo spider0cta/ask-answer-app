e7a2df1c686392add739654d2cf0769921e8ffe1
Urodacus <muhammadalaidy@protonmail.com>
2021-07-19 11:41:49 +0200

    pushing initial application skelton

---
+++ src/main/java/dev/muhammad/askanswerapp/AskAnswerAppApplication.java
@@ -0,0 +1,13 @@
+package dev.muhammad.askanswerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAnswerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskAnswerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/muhammad/askanswerapp/AskAnswerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muhammad.askanswerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskAnswerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
